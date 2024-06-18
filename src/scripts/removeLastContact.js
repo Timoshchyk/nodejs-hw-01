0a3ac59ea5ab5031276253f97860f3f07f4df149
Timoshchyk <timosuko202@gmail.com>
2024-06-18 23:04:36 +0300

    ndjs-hw-01

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -2,15 +2,22 @@ import { PATH_DB } from '../constants/contacts.js';
 import fs from 'node:fs/promises';
 
 export const removeLastContact = async () => {
-  const data = await fs.readFile(PATH_DB, 'utf-8');
-  const myArray = JSON.parse(data);
-  const removeLastContact = myArray.filter(() => Math.random() >= 0.5);
+  try {
+    const data = await fs.readFile(PATH_DB, 'utf-8');
+    const myArray = JSON.parse(data);
 
-  const update = JSON.stringify(removeLastContact, null, 2);
+    if (myArray.length > 0) {
+      myArray.pop();
+    }
 
-  await fs.writeFile(PATH_DB, update + '\n');
+    const update = JSON.stringify(myArray, null, 2);
 
-  console.log(removeLastContact);
+    await fs.writeFile(PATH_DB, update + '\n');
+
+    console.log('Updated contacts:', myArray);
+  } catch (error) {
+    console.error('Error removing last contact:', error);
+  }
 };
 
 await removeLastContact();
